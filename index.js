5e134ac6f6428ad81e74fe88d9f322c7d0788868
chobkyu <qudqud97@naver.com>
2022-06-23 15:54:31 +0900

    2022-06-23 15:54
    주석 추가

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require('express')//민수야 공부하자
+const express = require('express')//node_modules에 있는 express 관련 파일 가져옴
 
-const app = express();
+const app = express();//express 함수 변환 값 변수에 저장
 
 app.get('/', function(req, res){
     res.sendFile(__dirname+'/index.html');
-});
-app.listen(8080, function(){
+}); //여기서 html 파일 가져오는 듯
+
+app.listen(8080, function(){//8080 포트로 서버 오픈
     console.log('server start on 8080');
 });
