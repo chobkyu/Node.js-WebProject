3bebfb2aa937d99cf27983c8cb97b90096d589f8
chobkyu <qudqud97@naver.com>
2022-06-27 16:07:50 +0900

    2022-06-27 16:07

--- app/app.js
+++ app/app.js
@@ -12,6 +12,8 @@ const home = require("./src/routes/home");
 //앱 세팅
 app.set("views","./src/views");
 app.set("view engine","ejs"); //html 코드를 어떤 엔진으로 해석할지
+app.use(express.static(__dirname+'/src/public'));
+//폴더 경로 __dirname으로 해야되는데 안되서 일단 이렇게 해둠
 
 
 app.use("/",home); //use -> 미들 웨어를 등록해주는 메서드
