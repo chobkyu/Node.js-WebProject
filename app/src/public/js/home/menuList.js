8f89d0c36889cbd34bea086a02b4d88bd5efdcb8
chobkyu <qudqud97@naver.com>
2022-07-17 01:13:29 +0900

    2022-07-17 01:13

--- app/src/public/js/home/menuList.js
+++ app/src/public/js/home/menuList.js
@@ -1,11 +1,32 @@
 "use strict";
-
+/*
 const menu = document.querySelector("#menu"),
     price = document.querySelector("#price"),
     menuBtn = document.querySelector("#menuBtn");
 
 
-menuBtn.addEventListener("click",menuSel);
+menu.addEventListener("click",menuSel);
+*/
+const pay = document.querySelector('#pay');
+const basketList = new Array();
+
+pay.addEventListener("click", ()=>{
+    if (confirm("결제하시겠습니까?") == true){ 
+        //true는 확인버튼을 눌렀을 때 코드 작성
+        var payMoney = 0;  //총 금액
+        
+        for(var i=0; i<basketList.length;i++){
+            console.log(parseInt(basketList[i].menuprice));
+            payMoney = payMoney + parseInt(basketList[i].menuprice);
+        }
+        
+        console.log(payMoney);
+      
+    }else{
+        // false는 취소버튼을 눌렀을 때, 취소됨
+        console.log("취소되었습니다");
+      }
+})
 
 function category(menuSelect){
     
@@ -17,13 +38,17 @@ function category(menuSelect){
     location.href = "/menu?sel="+menuSelect;
 }
 
-function menuSel(){
-    const basketList = new Array();
+function menuSel(menu, price){
+    console.log(menu);
+    console.log(price);
+    
     
     var menuSel = {
-        name : menu.innerHTML,
-        menuprice : price.innerHTML,
+        name : menu,
+        menuprice : price,
     }
 
-    console.log(menuSel);
+    basketList.push(menuSel);
+    
+    
 }
