89f19dc69dd5f4e9886ebc360bfddcd24c7f7734
chobkyu <qudqud97@naver.com>
2022-07-19 00:45:16 +0900

    2022-07-19 00:45

--- app/app.js
+++ app/app.js
@@ -20,6 +20,13 @@ app.use(express.json());
 //url을 통해 전달되는 테이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(express.urlencoded({extended : true}));
 
+app.use( // 미들웨어 적용
+  session({
+    secret: "@haAdvanced",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 //app.get(routes.getpost, getpost);
 
