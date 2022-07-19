fc8f103dc96c7d83144a94b052ce5869ffd10a7e
chobkyu <qudqud97@naver.com>
2022-07-19 22:20:27 +0900

    2022-07-19 22:20
    일단 세션 주석처리

--- app/app.js
+++ app/app.js
@@ -15,18 +15,19 @@ const home = require("./src/routes/home");
 //앱 세팅
 app.set("views","./src/views");
 app.set("view engine","ejs"); //html 코드를 어떤 엔진으로 해석할지
-app.use(express.static(__dirname+'/src/public'));
-app.use(express.json());
-//url을 통해 전달되는 테이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
-app.use(express.urlencoded({extended : true}));
-
-app.use( // 미들웨어 적용
+/*app.use( // 미들웨어 적용
   session({
     secret: "@haAdvanced",
     resave: false,
     saveUninitialized: true,
   })
-);
+);*/
+app.use(express.static(__dirname+'/src/public'));
+app.use(express.json());
+//url을 통해 전달되는 테이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
+app.use(express.urlencoded({extended : true}));
+
+
 
 //app.get(routes.getpost, getpost);
 
