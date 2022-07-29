a7d09e1f884fdaccb87add86a2e0b1513cc4e96a
gunho30811 <gunho30811@naver.com>
2022-07-29 14:50:31 +0900

    2022-07-29 14:49
    menu페이지 장바구니 작업
    ->미완 추가 할 예정

--- app/src/public/js/home/menuList.js
+++ app/src/public/js/home/menuList.js
@@ -10,7 +10,20 @@ menu.addEventListener("click",menuSel);
 const pay = document.querySelector('#pay');
 const basketList = new Array();
 
-pay.addEventListener("click", ()=>{
+function pay_Money(){
+
+    if(confirm("결제하시겠습니까?") ==true){
+        var payMoney =0;
+    }
+    for(var i=0; i<basketList.length;i++){
+        console.log(parseInt(basketList[i].menuprice));
+        payMoney=payMoney+parseInt(basketList[i].menuprice);
+    }
+    console.log(payMoney);
+
+}
+
+/*pay.addEventListener("click", ()=>{
     if (confirm("결제하시겠습니까?") == true){ 
         //true는 확인버튼을 눌렀을 때 코드 작성
         var payMoney = 0;  //총 금액
@@ -26,7 +39,7 @@ pay.addEventListener("click", ()=>{
         // false는 취소버튼을 눌렀을 때, 취소됨
         console.log("취소되었습니다");
       }
-})
+})*/
 
 function category(menuSelect){
     
@@ -35,8 +48,20 @@ function category(menuSelect){
     /*if(menuSelect===""){
         location.href = "/menu?sel=한식";
     }*/
-    
     location.href = "/menu?sel="+menuSelect;
+
+}
+function 임시(){
+   const a =basketList.length;
+    for(var i=0;i<a;i++){
+        //document.querySelector('div.navbar').innerHTML=basketList[i].name;
+        console.log(basketList.length);
+        document.querySelector('div.basket').innerHTML=(basketList[i].name);
+        "<br>";
+        console.log(basketList.length);
+    }
+   /* console.log(basketList.length);
+    console.log(1);*/
 }
 
 function menuSel(menu, price){
@@ -51,5 +76,15 @@ function menuSel(menu, price){
 
     basketList.push(menuSel);
     
+    임시();
+
     
+}
+
+function fetch_page(name){
+    fetch('menu?sel='+name).then(function(response){
+        response.text().then(function(text){
+            document.querySelector('body').innerHTML=text;
+        })
+    })
 }
