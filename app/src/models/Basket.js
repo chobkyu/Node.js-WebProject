23d0793bd5878492570d5fcb8338cd8e4887ce8d
cxzaqq <roy1228@naver.com>
2022-09-03 21:39:07 +0900

    2022-09-03 21:39

--- app/src/models/Basket.js
+++ app/src/models/Basket.js
@@ -7,33 +7,71 @@ class Basket{
         this.body = body;
     }
 
-async divideOrder(){
-    const rows = await BasketStorage.selectOrder();
-    const orderLen = rows.length-1;
-    const last = rows[orderLen].orderNum;
-    console.log(last);
+    async divideOrder(){
+        const rows = await BasketStorage.selectOrder();
+        const orderLen = rows.length-1;
+        const last = rows[orderLen].orderNum;
+        console.log(last);
 
-    const orderRows = new Array();
-    for(var i = 1; i<=last; i++){
-        const temp = new Array();
-        for(var j=0; j<=orderLen; j++){
-            var data = new Object;
-            if(i===rows[j].orderNum){
-                data.menu = rows[j].menu;
-                data.orderNum = rows[j].orderNum;
-                data.seq = rows[j].seq;
-                if(data.seq!==undefined){
-                    temp.push(data);
+        const orderRows = new Array();
+        for(var i = 1; i<=last; i++){
+            const temp = new Array();
+            for(var j=0; j<=orderLen; j++){
+                var data = new Object;
+                if(i===rows[j].orderNum){
+                    data.menu = rows[j].menu;
+                    data.orderNum = rows[j].orderNum;
+                    data.seq = rows[j].seq;
+                    if(data.seq!==undefined){
+                        temp.push(data);
+                    }
                 }
             }
+            console.log(temp.length);
+            if(temp.length>0){
+                orderRows.push(temp);
+            }
         }
-        console.log(temp.length);
-        if(temp.length>0){
-            orderRows.push(temp);
+    return orderRows;
+    }
+
+    async delete(seq){
+        try{
+            const response = await BasketStorage.delete(seq);
+            return response;
+        }catch (err) {
+            return {success : false, msg : err};
         }
     }
-    return orderRows;
-}
+
+    async order(){
+        const order = this.body;
+        const num = order.length;
+        var orderSeq=0;
+
+        try{
+            const response = await BasketStorage.Order(orderSeq);
+            return response;
+        }catch (err) {
+            return {success : false, msg : err};
+        }
+    }
+
+    async insertOrder(){
+        const order = this.body;
+        const num = order.length;
+        var orderSeq = await this.order();
+        orderSeq = orderSeq+1;
+        console.log(orderSeq);
+
+        try{
+            const response = await BasketStorage.insertOrder(order, num, orderSeq);
+            return response;
+        }catch (err) {
+            return {success : false, msg : err};
+        }
+    }
+
 }
 
 module.exports = Basket;
--- app/src/models/basketStorage.js
+++ app/src/models/basketStorage.js
@@ -7,11 +7,7 @@ class BasketStorage{
         this.body = body;
     }
 
-    Order(){
-        const order = this.body;
-        const num = order.length;
-        var orderSeq=0;
-        
+    static async Order(orderSeq){
         return new Promise((resolve,reject) =>{
             const query ="SELECT orderNum FROM basket ORDER BY orderNum DESC LIMIT 1";
             db.query(query,(err, seq)=>{
@@ -19,101 +15,44 @@ class BasketStorage{
                     reject("${err}");
                     console.log("basketstorage에서 오류");
                 }
-                console.log("num : " + seq[0].orderNum);
                 orderSeq = seq[0].orderNum;
-                console.log(orderSeq);
                 resolve(orderSeq);
-            
             });
         });
         
     }
     
-           
-      
-    async insertOrder(){   
-        const order = this.body;
-        const num = order.length;
-        var orderSeq = await this.Order();
-        orderSeq = orderSeq+1;
-        console.log(orderSeq);
-        
-        try{
-            return new Promise((resolve, reject) =>{
-                const query = "insert into basket(menu,price,orderNum) values(?,?,?);";
-                for(var i = 0; i<num ; i++){
-                    console.log(order[i].name);
-                    db.query(query,[order[i].name,order[i].price,orderSeq], (err) =>{
-                        if(err) reject("${err}");
-                        resolve({success : true});
-                    })
-                }
-               
-            });
-        }catch(err){
-            return {success : false, msg : err};
-        }
-            
+    static async insertOrder(order, num, orderSeq){
+        return new Promise((resolve, reject) =>{
+            const query = "insert into basket(menu,price,orderNum) values(?,?,?);";
+            for(var i = 0; i<num ; i++){
+                console.log(order[i].name);
+                db.query(query,[order[i].name,order[i].price,orderSeq], (err) =>{
+                    if(err) reject("${err}");
+                    resolve({success : true});
+                })
+            }
+        });
     }
 
     static async selectOrder(){
-        
         return new Promise((resolve, reject)=>{
             const query = "select * from basket";
             db.query(query,(err,row) => {
             if(err) reject("${err}");
             resolve(row);
-                    
             })
         })
-    
     }
 
-    // async divideOrder(){
-    //     const rows = await this.selectOrder();
-    //     const orderLen = rows.length-1;
-    //     const last = rows[orderLen].orderNum;
-    //     console.log(last);  //마지막 주문 번호
-    //     //console.log(rows);
-    //     const orderRows = new Array();
-    //     for(var i = 1; i<=last;i++){  //4번 반복
-    //         const temp = new Array();
-            
-    //         for(var j=0 ;j<=orderLen;j++){  //데이터 개수만큼 반복
-    //             var data = new Object;
-    //             if(i===rows[j].orderNum){
-    //                 //console.log(rows[j].menu + " "+i);
-    //                 data.menu = rows[j].menu;
-    //                 data.orderNum = rows[j].orderNum;
-    //                 data.seq = rows[j].seq;
-    //                 if(data.seq!==undefined){
-    //                     temp.push(data);
-    //                 }
-                    
-    //             }
-    //         }
-    //         console.log(temp.length);
-    //         if(temp.length>0){
-    //             orderRows.push(temp);
-    //         }
-           
-    //     }
-    //     //console.log(orderRows);
-    //     return orderRows;
-    // }
-
-    delete(seq){
-        try{
-            return new Promise((resolve, reject) => {
-                const query = "delete from basket where orderNum=?;";
-                db.query(query, seq, (err) =>{
-                    if(err) reject("${err}");
-                    resolve({success : true});
-                })
-            });
-        }catch (err) {
-            return {success : false, msg : err};
-        }
+    static async delete(seq){
+        return new Promise((resolve, reject) => {
+            const query = "delete from basket where orderNum=?;";
+            db.query(query, seq, (err) =>{
+                if(err) reject("${err}");
+                resolve({success : true});
+            })
+        });
     }
 }
 
